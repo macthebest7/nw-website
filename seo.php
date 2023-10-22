0d4458b0f6ed331a788e0063e27265d909d86024
macthebest7 <118048030+macthebest7@users.noreply.github.com>
2023-10-23 02:17:04 +0500

    Add files via upload

---
+++ seo.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Novawave LLC</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:Novawavellc.wy@gmail.com">Novawavellc.wy@gmail.com</a></i>
+        
+      </div>
+      <div class="contact-info d-flex d-md-flex align-items-center">
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 (716) 226-0855</span></i>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="index.php">CMP</a></h1>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li class="dropdown"><a href="#"><span>Development Services</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="web.php">Web development</a></li>
+              <li><a href="seo.php">SEO</a></li>
+              <li><a href="goole_maps.php">Google Maps Services</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>SEO</li>
+        </ol>
+        <h2>SEO</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Our Skills Section ======= -->
+    <section id="skills" class="skills">
+      <div class="container">
+
+        <div>
+          <h3>Novawave SEO Services: Amplifying Your Online Presence, Elevating Your Brand</h3>
+          <p>Welcome to Novawave SEO Services, where your digital visibility is our priority. In an age where online competition is fierce, our team of skilled SEO experts is dedicated to optimizing your website's performance, boosting its search engine rankings, and ensuring your brand stands out in the digital landscape.</p>
+
+          <h3>Unlocking Organic Growth</h3>
+          <p>Search engine optimization (SEO) is more than just technical jargon – it's the key to unlocking organic growth for your business. At Novawave SEO Services, we employ proven strategies to enhance your website's visibility on search engines like Google. This translates into increased traffic, higher conversions, and a stronger online presence.</p>
+
+          <h3>Tailored Strategies, Tangible Results</h3>
+          <p>Every business is unique, and so are its SEO needs. Our approach is centered around crafting tailored strategies that align with your specific goals. Whether you're aiming to improve local visibility, increase e-commerce sales, or establish industry authority, we customize our tactics to deliver tangible results that matter to you</p>
+
+          <h3>On-Page and Off-Page Excellence</h3>
+          <p>Our comprehensive SEO services encompass both on-page and off-page optimization. From optimizing meta tags and content to enhancing site speed and mobile-friendliness, we ensure your website is in top shape to impress both users and search engines. Additionally, we focus on building high-quality backlinks and online authority to boost your site's credibility.</p>
+
+          <h3>Transparency and Collaboration</h3>
+          <p>At Novawave SEO Services, transparency is paramount. We believe in keeping you informed every step of the way. Our collaborative approach involves regular communication and updates on the progress of your SEO campaign. We're not just consultants; we're partners invested in your digital success.</p>
+
+          <h3>Experience SEO Excellence</h3>
+          <p>Choosing Novawave SEO Services means choosing a partner dedicated to amplifying your online reach and elevating your brand to new heights. Experience the power of strategic SEO that drives meaningful results and positions your business as a force to be reckoned with in the digital world.</p>
+        </div>
+ 
+      </div>
+    </section><!-- End Our Skills Section -->
+
+  </main><!-- End #main -->
+
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="refund.php">Refund policy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="terms.php">Term Of Use</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="privacy.php">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="web.php">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="seo.php">SEO</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="goole_maps.php">Google Maps</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              30N Gould St Ste 33671 <br>
+              Sheridan, WY 82801<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 (909) 281-2331<br>
+              <strong>Email:</strong> info@Novawavellc.wy.com<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>Novawave</h3>
+            <p>In a world driven by technology, challenges can arise at any moment. That's where Novawave comes in. Our tech assistance services are designed to provide quick, effective solutions to keep your digital world running smoothly.</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Novawave LLC</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://Novawavellc.wy.com/">Novawave LLC</a><br>
+        <img src="img/payment.png">
+      </div>
+    </div>
+
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script></body>
+
+</html>
